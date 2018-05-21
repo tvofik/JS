90df71519e6a8362cc6386a775439ea154e76508
Abayomi Pelewura <abayomi_pelewura@yahoo.co.uk>
2018-05-21 11:34:49 -0400

    Add 'this' Keyword

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -1,21 +1,27 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+/*
+calculateAge(1965);
 
+function calculateAge(year) {
+  console.log(2016 - year);
+}
 
+// retirement(1990);
+var retirement = function (year) {
+  console.log(65 - (2016 - year) );
+}
 
+// variables
+var age = 23;
 
-
-
-
-
-
-
-
-
-
-
-
-
+function foo() {
+  var age = 65;
+  console.log(age);
+}
+foo();
+console.log(age);
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -37,7 +43,6 @@ function first() {
     }
 }
 */
-
 
 
 // Example to show the differece between execution stack and scope chain
@@ -67,11 +72,41 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+
+/*
+
+function calculator(year) {
+  console.log(2016 - year);
+  console.log(this);
+}
+
+calculator(1990);
 
 
+var john = {
+  name: 'John',
+  yob: 1990,
+  calculateAge: function (){
+    console.log(this);
+    console.log(2016 - this.yob);
+    
+    function innerFunction() {
+      console.log(this);
+    }
+    innerFunction();
+  }
+};
+
+john.calculateAge();
 
 
+var mike = {
+  name: 'Mike',
+  yob: 1984
+};
 
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
-
-
+*/
